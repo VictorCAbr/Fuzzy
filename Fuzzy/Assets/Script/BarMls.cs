62c18aaaab7aa41fe068b0fd831869650a792ef9
VictorCAbr <victorcdeabrantes@gmail.com>
2019-11-03 12:14:40 -0300

    Barras com Fuzzy

--- Fuzzy/Assets/Script/BarMls.cs
+++ Fuzzy/Assets/Script/BarMls.cs
@@ -8,13 +8,18 @@ public class BarMls : MonoBehaviour
     private Image Mistura;
     private Image Maior;
     private Text TxtQntidade;
-    [Range(0, 255)]
-    public float RGBr;
+    public Vector2 FxFaixa = new Vector2(0, 100);
+    public Vector4 FxForte;
+    public Vector4 FxSuave;
+    public Vector4 FxFraco;
+
     [Range(0, 100)]
+    public float MLS;
+    [Range(0, 1)]
     public float Forte;
-    [Range(0, 100)]
+    [Range(0, 1)]
     public float Suave;
-    [Range(0, 100)]
+    [Range(0, 1)]
     public float Fraco;
     public Color CorT;
     public Color CorM;
@@ -27,15 +32,45 @@ public class BarMls : MonoBehaviour
         TxtQntidade = transform.GetChild(2).GetChild(0).GetChild(0).GetComponent<Text>();
         CorT = Color.white;
         CorM = Color.white;
+        GetComponent<Slider>().minValue = FxFaixa.x;
+        GetComponent<Slider>().maxValue = FxFaixa.y;
 }
 
     // Update is called once per frame
     void Update()
     {
-        RGBr = (int)RGBr;
         MisturarCores();
-        
-        TxtQntidade.text = "" + RGBr;
+
+        Forte = Pertinencia(FxForte);
+        Suave = Pertinencia(FxSuave);
+        Fraco = Pertinencia(FxFraco);
+
+        TxtQntidade.text = "" + (int) MLS;
+    }
+    float Pertinencia(Vector4 Fx)
+    {
+        float p = 0;
+        if (CorT==Color.white)
+        {
+            MLS = (int)MLS;
+        }
+
+        if (Fx.x < MLS && MLS <= Fx.y)
+        {
+            p = (MLS - Fx.x) / (Fx.y - Fx.x);
+        }
+        else if (Fx.y <= MLS && MLS <= Fx.z)
+        {
+            p = 1;
+        }
+        else if (Fx.z < MLS && MLS <= Fx.w)
+        {
+            p = (Fx.w - MLS) / (Fx.w - Fx.z);
+        }
+
+
+
+        return p;
     }
     void MisturarCores()
     {
@@ -43,9 +78,9 @@ public class BarMls : MonoBehaviour
         Maior.color = CorM;
 
         
-        CorT.r = Forte / 100;
-        CorT.g = Suave / 100;
-        CorT.b = Fraco / 100;
+        CorT.r = Forte;
+        CorT.g = Suave;
+        CorT.b = Fraco;
         CorT.a = 1;
         if (CorT == Color.black)
             CorT = Color.white;
@@ -62,6 +97,8 @@ public class BarMls : MonoBehaviour
     }
     public void QantidadeML(float Value)
     {
-        RGBr = Value;
+        MLS = Value;
+        if (MLS == FxFaixa.x)
+            MLS = 0;
     }
 }
